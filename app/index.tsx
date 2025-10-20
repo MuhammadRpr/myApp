b8f80159b2aeee3924f9e777527d307fc5208a2c
MuhammadRpr <muhammadqu017@gmail.com>
2025-10-20 21:12:00 +0700

    up lgi

--- app/index.tsx
+++ app/index.tsx
@@ -4,21 +4,21 @@ export default function Index() {
   return (
     <View>
       <Text>muhammad</Text>
-      isi nama
+      <Text>isi nama</Text>
       <TextInput placeholder="nama"  style={{
           height: 40,
           padding: 5,
           marginHorizontal: 8,
           borderWidth: 1,
         }}/>
-        isi kelas
+        <Text>isi kelas</Text>
       <TextInput placeholder="kelas"  style={{
           height: 40,
           padding: 5,
           marginHorizontal: 8,
           borderWidth: 1,
         }}/>
-        isi hobi
+        <Text>isi hobi</Text>
       <TextInput placeholder="hobi"  style={{
           height: 40,
           padding: 5,
